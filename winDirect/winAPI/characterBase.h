071d7f99ef2fe52d80a07a5fdf679f1b4133646e
RECTAL <33717935+RECTAL@users.noreply.github.com>
2018-10-25 14:34:25 +0900

    오류 수정

--- winDirect/winAPI/characterBase.h
+++ winDirect/winAPI/characterBase.h
@@ -7,28 +7,40 @@
 
 
 
-class characterBase:public baseObject
+class characterBase :public baseObject
 {
 protected:
-	patternBase * _currentPattern;
+	patternBase * _currentPattern;				//현재 패턴
 
-	list<buffBase*> _LbuffList;
+	list<buffBase*> _LbuffList;					//버프 리스트
 	list<buffBase*>::iterator _Libuff;
 
 protected:
-	fPOINT	_velocity;
-	state	_state;
+	fPOINT		_velocity;						//속도			<- 중력 + 마찰속도 + 현재 속도 (최종속도)
+	RAYSTRUCT	_rayStruct;						//광선 구조체(충돌검사 광선)
+
+	bool		_isCollision;					//충돌검사 여부 변수
+protected:
+	void setRayStruct();						//광선 생성 움직일때 마다 생성됨
+	void rayCollision();						//광선 충돌
+
 public:
 
 	void setPattern(patternBase* pattern) { _currentPattern = pattern; }
-	
+
 	list<buffBase*>&getBuffList() { return _LbuffList; }
 	patternBase* getPattern() { return _currentPattern; }
+	void getVelocity() {
+		_velocity = _currentPattern->getPatternVelocity();
+		_velocity.y += 1.0f;
 
+		setRayStruct();
+		rayCollision();
+	}
 
-	void characterMove() { _position.x += _velocity.x; _position.y += _velocity.y; };
-
-
+	virtual HRESULT init() = 0;
+	virtual void release() = 0;
+	virtual void render() = 0;
 	characterBase() {};
 	~characterBase() {};
 };
